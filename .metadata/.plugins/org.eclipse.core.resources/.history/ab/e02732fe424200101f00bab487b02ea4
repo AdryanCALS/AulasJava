6b3cf35c58cf8b6e95f4b6eecb386cdac3d42ce8
Adryan Lima <acals@poli.br>
2025-06-09 13:43:44 -0300

    Aula01

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/ab/e02732fe424200101f00bab487b02ea4
@@ -0,0 +1,16 @@
+package aula01;
+
+public class Exercicio05 {
+
+	public static void main(String[] args) {
+		int idade=15;
+		boolean amigodoDono = true;
+		if(idade<18 && amigodoDono ==false) {
+			System.out.println("não pode entar");
+		}else{
+			System.out.println("Pode entrar");
+		}
+
+	}
+
+}
---
+++ aula01/src/aula01/Aula01.java
@@ -0,0 +1,17 @@
+package aula01;
+
+public class Aula01 {
+	public static void main(String[] args) {
+		/*
+		 * int idade = 80; double media; media = 8.75; final double pi = 3.141592; byte
+		 * num = 9; short caixas = 1000; long valor = 333333333; float valor2 = (float)
+		 * 4.6; char valor3 = 256; char valor4 = 'a'; String texto = "texto da string";
+		 * System.out.println("atalho -> syso+ctrlspaço");
+		 */
+		int x = 10;
+		for (int i =0;i<=x;i++) {
+			System.out.printf("O valor de i é: %,d%n", i);
+		}
+		
+	}
+}
